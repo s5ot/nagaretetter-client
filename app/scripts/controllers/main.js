96e4a9196bf928b71a579bd210e8688d1ae92c51
sogo.ohta <sogo.ohta@gmail.com>
2014-03-14 02:11:10 +0900

    Modify

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -155,4 +155,23 @@ angular.module('nagaretetter')
             }
         });
     };
-});
+})
+.directive('twitter', [
+    function() {
+        return {
+            link: function(scope, element, attr) {
+                setTimeout(function() {
+                        twttr.widgets.createShareButton(
+                            attr.url,
+                            element[0],
+                            function(el) {}, {
+                                count: 'none',
+                                text: attr.text,
+                                hashtags: attr.hashtags
+                            }
+                        );
+                });
+            }
+        }
+    }
+]);
