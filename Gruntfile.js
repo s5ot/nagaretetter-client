4e04ddfeb2fc36f38284e1acafb042114627250b
sogo.ohta <sogo.ohta@gmail.com>
2015-03-13 02:16:24 +0900

    Modify

--- Gruntfile.js
+++ Gruntfile.js
@@ -349,6 +349,21 @@ module.exports = function (grunt) {
         configFile: 'karma.conf.js',
         singleRun: true
       }
+    },
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:s5ot/nagaretetter-client.git',
+          branch: 'gh-pages'
+        }
+      }
     }
   });
 
